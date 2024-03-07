3e49d0e65fe5c43fb7efa33c14a247adcd826f32
PrabavathiM <pprabavathi539@gmail.com>
2024-03-07 21:09:44 +0530

    update the files

--- src/app/components/layout/HomeMenu.js
+++ src/app/components/layout/HomeMenu.js
@@ -1,18 +1,32 @@
 export default function HomeMenu() {
     return (
-        <section className="mt-20 relative">
+        <section className="mt-20 relative h-dvh">
 
-            <div className=" text-center">
+            <div className=" text-center overflow-x-hidden">
                 <h3 className="uppercase text-gray-500 semi-bold">check out</h3>
-                <h2 className="">Menu</h2>
+                <h2 className="text-primary font-semibold">Menu</h2>
             </div>
+        <br></br>
+            <div className="grid grid-cols-3 gap-4">
+                <div className="bg-gray-300 p-4 rounded-lg text-center">
+                    <div>
+                        <img src="R.jpg" alt="biryani" />
+                    </div>
 
-            <div className="w-40 h-40 relative">
-                <img src={'left.jpeg'} layout={'fill'} objectFit={'contain'} alt={'chicken-leg'} />
-            </div>
+                    <h2 className="text-5px font-semibold my-4">chicken Biryani</h2>
+                    <p className="text-sm">Chicken Biryani is a highly aromatic, mouth-watering staple dish that needs no introduction.</p>
+                    <button className="bg-primary text-white rounded-full py-3 px-2 m-2 ">
+                        Add to cart &#8377;100
+                    </button>
+                </div>
 
-            <div className="w-40 h-40 relative flex flex-direction-row  -right-20 ">
-                <img src={'chi-fry.jpeg'} layout={'fill'} objectFit={'contain'} alt={'chicken-fry'} />
+                <div className="bg-gray-300 p-4 rounded-lg text-center">
+                    <img alt="food" />
+                </div>
+
+                <div className="bg-gray-300 p-4 rounded-lg text-center">
+                    <img alt="food" />
+                </div>
             </div>
         </section>
     );
