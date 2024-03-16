fdde51c488ed1fa3e3af0595ff91a0f488269a63
PrabavathiM <pprabavathi539@gmail.com>
2024-03-16 19:02:02 +0530

    db files updates

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Header from 'src/components/layout/Header';
+import Hero from 'src/components/layout/Hero';
+import HomeMenu from 'src/components/layout/HomeMenu';
+
+
+
+export default function Home() {
+  return (
+
+    <>
+      <Header />
+      <Hero />
+      <HomeMenu />
+
+    </>
+
+  );
+}
